9796ccd713231c9f3667bedfb17b1bd701a752b6
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-10 15:43:05 +0100

    start with the basics

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel():
         """"initialization"""
 
         self.id = str(uuid4())
-        self.created_at = datetime.now
-        self.updated_at = datetime.now
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """"print"""
@@ -22,9 +22,11 @@ class BaseModel():
 
     def save(self):
         ''''updates the public instance attribute'''
-        self.updated_at = datetime.now
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """"returns a dictionary containing all keys/values"""
         self.__dict__['__class__'] = self.__class__.__name__
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['id'] = str(uuid4())
         self.__dict__['created_at'] = self.created_at.isoformat()
