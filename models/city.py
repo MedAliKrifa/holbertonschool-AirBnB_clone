a1651744b91125717881d535847008ac2fe5121e
MedAliKrifa <daliolimona@gmail.com>
2022-10-12 22:43:31 +0100

    models/

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""""city module"""
+from models.base_model import BaseModel
+
+
+class State (BaseModel):
+    """city class"""
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""review module"""
+from models.base_model import BaseModel
+
+
+class State (BaseModel):
+    """review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
