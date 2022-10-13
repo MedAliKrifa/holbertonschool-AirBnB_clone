e00f469d5a7ef5163c9305dab6067e75c13002f1
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-13 09:46:21 +0100

    fixing

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""""that contains the entry point of the command interpreter"""
+
+
+import cmd
+
+
+
+class HBNBCommand(cmd.Cmd):
+    """"console class"""
+
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, arg):
+        '''exit the program'''
+
+        return True
+
+    def do_quit(self, arg):
+        ''''exit the program'''
+
+        return True
+
+    def emptyline(self, arg):
+        """"empty like my heart"""
+
+        pass
+
+    
